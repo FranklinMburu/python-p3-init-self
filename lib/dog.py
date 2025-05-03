b7d877ca97f4654b2ad8c66f2c0c25ffbff37673
FranklinMburuDev <184284364+FranklinMburu@users.noreply.github.com>
2025-05-03 19:55:44 +0300

    Implement Person and Dog classes with appropriate initialization and attributes

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
-class Dog:
-    pass
+class Person:
+    def __init__(self, name):
+        self.name = name
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 
-class Person:
-    pass
+class Dog:
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
